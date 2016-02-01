33dba3291eb4a476a463af7dc45ba4555cf67b28
Md Bulbul Hossen <info247.bulbul@gmail.com>
2016-02-02 02:58:39 +0600

    ExplosionField animation

--- app/src/main/java/com/example/appsd/loadingindicatorviewbest/MainActivity.java
+++ app/src/main/java/com/example/appsd/loadingindicatorviewbest/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.appsd.loadingindicatorviewbest;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import tyrantgit.explosionfield.ExplosionField;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ExplosionField mExplosionField;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mExplosionField = ExplosionField.attach2Window(this);
+        addListener(findViewById(R.id.root));
+    }
+
+
+    private void addListener(View root) {
+        if (root instanceof ViewGroup) {
+            ViewGroup parent = (ViewGroup) root;
+            for (int i = 0; i < parent.getChildCount(); i++) {
+                addListener(parent.getChildAt(i));
+            }
+        } else {
+            root.setClickable(true);
+            root.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mExplosionField.explode(v);
+                    v.setOnClickListener(null);
+                }
+            });
+        }
+    }
+
+
+
+
+    private void reset(View root) {
+        if (root instanceof ViewGroup) {
+            ViewGroup parent = (ViewGroup) root;
+            for (int i = 0; i < parent.getChildCount(); i++) {
+                reset(parent.getChildAt(i));
+            }
+        } else {
+            root.setScaleX(1);
+            root.setScaleY(1);
+            root.setAlpha(1);
+        }
     }
 }
